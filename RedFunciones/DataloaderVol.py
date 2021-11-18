a16970addfa87147cce9dab15ffd39fd70963c78
root <ChrisColabDrive>
2021-11-18 03:20:40 +0000

    "Add model view and h5 in net"

--- RedFunciones/DataloaderVol.py
+++ RedFunciones/DataloaderVol.py
@@ -10,7 +10,7 @@ import pandas as pd
 class VolcanoDatasetH5(Dataset):
 
     def __init__(self, in_file, transform=None):
-        super(VolcanoDataset, self).__init__()
+        # super(VolcanoDatasetH5, self).__init__()
         self.file = h5py.File(in_file, 'r')
         self.transform = transform
 
@@ -20,8 +20,7 @@ class VolcanoDatasetH5(Dataset):
         # Preprocessing each image
         if self.transform is not None:
             x = self.transform(sample)
-
-        return sample, tag
+        return sample, int(tag)
 
     def __len__(self):
         return self.file['X_train'].shape[0]
@@ -30,7 +29,7 @@ class VolcanoDatasetH5(Dataset):
 class VolcanoDataset(Dataset):
 
     def __init__(self, dir_name, json_name):
-        super(VolcanoDataset, self).__init__()
+        # super(VolcanoDataset, self).__init__()
         self.dir_name = dir_name
         self.json_name = json_name
         with open(json_name, 'r') as fh:
@@ -50,6 +49,3 @@ class VolcanoDataset(Dataset):
         elif tipo == 'LP':
             tag = 1
         return sample, tag
-
-
-
