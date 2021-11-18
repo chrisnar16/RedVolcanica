cdd818ea0594b97efb249a11cbfca4220c8d1b59
root <chrisnar16>
2021-11-18 15:46:01 +0000

    Add save/load

---
+++ RedFunciones/Checkpoint.py
@@ -0,0 +1,25 @@
+from datetime import date
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+DIRECTORY='/content/drive/MyDrive/Tesis/Git/RedVolcanica/checkpoints/'
+
+
+def save_weighs(gen, disc, gen_opt, disc_opt, epoch, gen_loss, dis_loss, nombre):
+    today = date.today()
+    file_name = nombre + '-' + str(today) + '.pt'
+    compelto = DIRECTORY + file_name
+    file = open(compelto,'w+')
+    file.close()
+    torch.save({
+        'gen':gen.state_dict(),
+        'disc':disc.state_dict(),
+        'gen_opt': gen_opt.state_dict(),
+        'disc_opt': disc_opt.state_dict(),
+        'epoch': epoch,
+        'gen_loss': gen_loss,
+        'dis_loss': dis_loss,
+    }, compelto)
+        
