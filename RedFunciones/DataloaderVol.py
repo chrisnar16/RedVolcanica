7d40a018e4fa5a251a46f276618050d7853cf706
christian.yepez <christian.yepez@epn.edu.ec>
2021-11-17 20:16:24 -0500

    Add h5 support

--- RedFunciones/DataloaderVol.py
+++ RedFunciones/DataloaderVol.py
@@ -1,4 +1,5 @@
 import json
+import h5py
 
 import numpy as np
 import torch
@@ -6,9 +7,30 @@ from torch.utils.data import Dataset
 import pandas as pd
 
 
+class VolcanoDatasetH5(Dataset):
+
+    def __init__(self, in_file, transform=None):
+        super(VolcanoDataset, self).__init__()
+        self.file = h5py.File(in_file, 'r')
+        self.transform = transform
+
+    def __getitem__(self, index):
+        sample = self.file['X_train'][index, ...]
+        tag = self.file['Y_train'][index, ...]
+        # Preprocessing each image
+        if self.transform is not None:
+            x = self.transform(sample)
+
+        return sample, tag
+
+    def __len__(self):
+        return self.file['X_train'].shape[0]
+
+
 class VolcanoDataset(Dataset):
 
     def __init__(self, dir_name, json_name):
+        super(VolcanoDataset, self).__init__()
         self.dir_name = dir_name
         self.json_name = json_name
         with open(json_name, 'r') as fh:
