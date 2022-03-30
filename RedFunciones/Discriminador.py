231f3f60e39df3204cba0fa4efdf3f2279d48819
chrisnar16 <christian.yepez@epn.edu.ec>
2022-03-30 18:14:24 -0500

    RENACER

--- RedFunciones/Discriminador.py
+++ RedFunciones/Discriminador.py
@@ -13,10 +13,13 @@ class Discriminator(nn.Module):
     def __init__(self, im_chan=1, hidden_dim=64):
         super(Discriminator, self).__init__()
         self.disc = nn.Sequential( # 129, 33
-            self.make_disc_block(im_chan, hidden_dim, kernel_size=(7, 3), stride=(2, 2)),# 62, 16
-            self.make_disc_block(hidden_dim, hidden_dim * 4, kernel_size=(8, 3), stride=(2, 1)), # 28, 14
-            self.make_disc_block(hidden_dim * 4, hidden_dim * 8, kernel_size=(8, 2), stride=(2, 2)),# 11, 7
-            self.make_disc_block(hidden_dim * 8, 1, kernel_size=(11, 7), stride=(2, 2), final_layer=True),# 1, 1
+            self.make_disc_block(im_chan, hidden_dim, kernel_size=(13, 4), stride=(1, 1)),# 117, 30
+            self.make_disc_block(hidden_dim, hidden_dim * 2, kernel_size=(13, 4), stride=(2, 2)), # 53, 14
+            self.make_disc_block(hidden_dim * 2, hidden_dim * 4, kernel_size=(13, 4), stride=(1, 1)),# 41, 11
+            self.make_disc_block(hidden_dim * 4, hidden_dim * 8, kernel_size=(13, 4), stride=(1, 1)),# 29, 8
+            self.make_disc_block(hidden_dim * 8, hidden_dim * 16, kernel_size=(13, 4), stride=(1, 1)),# 17, 5
+            self.make_disc_block(hidden_dim * 16, hidden_dim * 32, kernel_size=(13, 4), stride=(1, 1)),# 5, 2
+            self.make_disc_block(hidden_dim * 32, 1, kernel_size=(5, 2), stride=(2, 2), final_layer=True),# 1, 1
         )
 
     def make_disc_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
